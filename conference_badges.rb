3af3b8e7aff9baf4636c23855e7a9adf534f267d
brenden williams <brendenw7@gmail.com>
2019-02-26 01:44:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,15 @@
 # Write your code here.
-let(:name) {"Arel"}
-  let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
-  let(:badges) {[
-                  "Hello, my name is Edsger.",
-                  "Hello, my name is Ada.",
-                  "Hello, my name is Charles.",
-                  "Hello, my name is Alan.",
-                  "Hello, my name is Grace.",
-                  "Hello, my name is Linus.",
-                  "Hello, my name is Matz."
-               ]}
+#let(:name) {"Arel"}
+#  let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+#  let(:badges) {[
+#                  "Hello, my name is Edsger.",
+#                  "Hello, my name is Ada.",
+#                  "Hello, my name is Charles.",
+#                 "Hello, my name is Alan.",
+#                  "Hello, my name is Grace.",
+#                  "Hello, my name is Linus.",
+#                  "Hello, my name is Matz."
+#               ]}
 
 
 
@@ -19,5 +19,6 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(attendees)
-
+  badges = Array.new
+  badges.push(attendees.each {|x| badge_maker(x)))
 end
