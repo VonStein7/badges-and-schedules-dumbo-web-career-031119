33d4f5a7b5a24ac9628421d2ada6526a9cfac027
brenden williams <brendenw7@gmail.com>
2019-02-26 01:41:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,13 @@
 # Write your code here.
+
+
+
+
+
 def badge_maker(name)
   attendee =  "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(attendees)
-  attendees.each {|x| x = badge_maker(x)}
-  attendees
+
 end
